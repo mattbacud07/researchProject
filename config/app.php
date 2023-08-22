18c10b2113927348148f81622a582680aa61795e
mattbacud <mattbacud@gmail.com>
2023-08-23 01:30:29 +0800

    edited the basepath

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://research.mattbacud.tech/'),
 
     'asset_url' => env('ASSET_URL'),
 
