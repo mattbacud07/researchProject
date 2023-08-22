c4db725767e3e7482c58c289565e0517fde6e0cd
mattbacud <mattbacud@gmail.com>
2023-08-23 04:08:16 +0800

    edited login page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,20 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>User Page for Development</h1>
+    <div class="mb-3">
+      <label for="" class="form-label">Name</label>
+      <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+      <small id="helpId" class="text-muted">Help text</small>
+    </div>
+    <div class="mb-3">
+      <label for="" class="form-label">Name</label>
+      <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+      <small id="helpId" class="text-muted">Help text</small>
+    </div>
+    <div class="mb-3">
+      <label for="" class="form-label">Name</label>
+      <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+      <small id="helpId" class="text-muted">Help text</small>
+    </div>
 </body>
 </html>
